048d06d3563df8eefac2adcc3cb32f1e882c1e51
Vicente Gutierrez <gtz.rvera@gmail.com>
2025-10-11 11:19:32 -0600

    feat: add shot action when pressing space

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import pygame
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 from constants import *
 
 
@@ -23,8 +24,10 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
--- player.py
+++ player.py
@@ -1,7 +1,8 @@
 import pygame
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from shot import Shot
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 
 
 class Player(CircleShape):
@@ -9,6 +10,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
 
         self.rotation = 0
+        self.shoot_timer = 0
 
     # in the player class
     def triangle(self):
@@ -32,6 +34,7 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def update(self, dt):
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -45,3 +48,15 @@ class Player(CircleShape):
 
         if keys[pygame.K_w]:
             self.move(dt)
+
+        if keys[pygame.K_SPACE]:
+            self.shot()
+
+    def shot(self):
+        if self.shoot_timer > 0:
+            return
+
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
+
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
